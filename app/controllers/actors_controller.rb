3eb84f757e5a5a7b64c0983dc17156387107b653
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-02-05 14:11:35 +0200

    actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,13 +1,69 @@
 class ActorsController < ApplicationController
-    def new
-        @actors = Actor.new()
-    end
+  before_action :set_actor, only: %i[show update edit destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-    def index
-        @actors = Actor.all
+  def new
+    @actor = Actor.new
+  end
+
+  def index
+    @actors = Actor.all
+  end
+
+  def show
+    render json: @actor
+  end
+
+  def edit
+    @actor = Actor.find(params[:id])
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+
+    respond_to do |format|
+      if @actor.save
+        format.json { render json: { actor: @actor, message: 'Actor created successfully' }, status: :created }
+      else
+        format.json { render json: { errors: @actor.errors.full_messages }, status: :unprocessable_entity }
       end
-    
-    def show
     end
-    
+  end
+
+  def update
+    respond_to do |format|
+      if @actor.update(actor_params)
+        format.json { render json: { actor: @actor, message: 'Actor updated successfully' }, status: :ok }
+      else
+        format.json { render json: { errors: @actor.errors.full_messages }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @actor.destroy
+        format.json { render json: { message: 'Actor deleted successfully' }, status: :ok }
+      else
+        format.json { render json: { errors: @actor.errors.full_messages }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def record_not_found
+    respond_to do |format|
+      format.json { render json: { error: "Actor with the id #{params[:id]} not found" }, status: :not_found }
+      format.any  { head :not_found }
+    end
+  end
+
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
+
+  def actor_params
+    params.require(:actor).permit(:first_name, :last_name)
+  end
 end
